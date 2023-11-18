37aa546eab5fac62a878228ab40a49afe9a6b642
raz <cloverofmisfortune04@gmail.com>
2023-11-18 20:38:15 +0200

    Changed .gitignore

--- Front-End/src/app/upload/page.js
+++ Front-End/src/app/upload/page.js
@@ -4,13 +4,13 @@ import Navbar from "../Navbar/Navbar"
 export default function UploadPage(){
 
     function handleFile(e){
-        console.log(e);
+        console.log(e.target.files);
     }
 
     return(
         <div className="w-[100vw] h-[100vh] bg-blue-900 flex flex-col">
                 <input type='text' placeholder="Numele documentului" className="min-w-[200px] w-[20%] h-[35px] p-[5px] text-black focus:outline-0 border-2 border-neutral-200 rounded-lg"></input>
-                <input type='file' onChangeCapture={handleFile} placeholder=""></input>
+                <input type='file' onCapture={handleFile} placeholder=""></input>
             <div id="background-gradient" className='z-0 invert'>
             </div>
         </div>
