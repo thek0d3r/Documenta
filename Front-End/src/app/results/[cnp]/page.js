06841baf02e0771cdb675bcbd0a1ec283a27ccdf
raz <cloverofmisfortune04@gmail.com>
2023-11-19 00:28:56 +0200

    quick log

--- Front-End/src/app/results/[cnp]/page.js
+++ Front-End/src/app/results/[cnp]/page.js
@@ -43,12 +43,12 @@ export default function Results({params}){
                         setLookUp({
                             id:json.id,
                             person:json.person,
-                            documents:response.json().documents
+                            documents:response.json()..documents
                         });
                         console.log({
                             id:json.id,
                             person:json.person,
-                            documents:response.json().documents
+                            documents:response.json()
                         })
                     })
                     console.log(json);
