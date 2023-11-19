319ec3d7671f4da01381c16a6c84f4b0700429f0
raz <cloverofmisfortune04@gmail.com>
2023-11-19 10:09:27 +0200

    UI Changes

--- Front-End/src/app/results/[cnp]/page.js
+++ Front-End/src/app/results/[cnp]/page.js
@@ -12,14 +12,14 @@ import { useEffect, useState } from "react";
 export default function Results({params}){
     const router=useRouter();
     const [lookUp, setLookUp]=useState({
-        // person:{
-        //     id:"NmNlYjRiN2EtNTMyNi00ZTRlLWFlMTEtOGY5ZDNkYmZlMjUy",
-        //     cnp:"5040525350048",
-        //     nume:"Szabo",
-        //     prenume:"Aleks"
-        // },
-        // id:"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252",
-        // "documents":{"cfc15005-af51-4632-9bd6-d5eca9f6d17b":{"id":"Y2ZjMTUwMDUtYWY1MS00NjMyLTliZDYtZDVlY2E5ZjZkMTdi","person":"VEVTVF9OQU1F","document_name":"TEST_HASH","document_hash":"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252"}}
+        person:{
+            id:"NmNlYjRiN2EtNTMyNi00ZTRlLWFlMTEtOGY5ZDNkYmZlMjUy",
+            cnp:"5040525350048",
+            nume:"Szabo",
+            prenume:"Aleks"
+        },
+        id:"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252",
+        "documents":{"cfc15005-af51-4632-9bd6-d5eca9f6d17b":{"id":"Y2ZjMTUwMDUtYWY1MS00NjMyLTliZDYtZDVlY2E5ZjZkMTdi","person":"VEVTVF9OQU1F","document_name":"TEST_HASH","document_hash":"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252"}}
     
     });
 
@@ -77,9 +77,30 @@ export default function Results({params}){
                     <div className="flex w-[100%] justify-center">
                         <SearchCNP/>
                     </div>
-                    <div className="flex w-[90%] flex-row items-center justify-between">
-                        <h3 className="text-black text-5xl font-['Helvetica'] font-bold m-10">{lookUp.person?.prenume}, {lookUp.person?.nume}</h3>
-                        <div className="bg-blue-900 flex flex-col items-center gap-[5px] rounded-lg p-[20px]">
+                    <div className="flex w-[90%] justify-between flex-row">
+                        <div className="flex w-[90%] flex-col justify-start justify-between">
+                            <h3 className="text-black text-5xl font-['Helvetica'] font-bold m-10">{lookUp.person?.prenume}, {lookUp.person?.nume}</h3>
+                            <section id="documents" className="flex flex-col mx-10 justify-center bg-gray-100 border-2 border-gray-200 p-[5px] rounded-lg mt-[10px]">
+                                {(lookUp.documents!=undefined)? Object.keys(lookUp.documents).map((e, index)=>{
+                                    let docName="document_name";
+                                    console.log(e, e["id"], e.docName, lookUp.documents, lookUp.documents[`${e}`][`${docName}`]);
+                                    if(index%2==0)
+                                        return(
+                                            <div key={index} className="flex w-[90%]">
+                                                <div className="flex items-center justify-center px-5 rounded-lg w-[40px] h-[40px] border-2 border-gray-300 text-center mr-[5px] text-blue-900 bg-neutral-200 mb-[5px] text-lg">{index}</div>
+                                                <div className="flex items-center px-5 rounded-lg  h-[40px] text-blue-900 bg-neutral-200 border-2 border-gray-300  mb-[5px] text-lg">{lookUp.documents[`${e}`][`${docName}`]}</div>
+                                            </div>
+                                        )
+                                    return(
+                                        <div key={index} className="flex w-[90%]">
+                                            <div className="flex items-center justify-center px-5 rounded-lg w-[40px] h-[40px] text-center mr-[5px] text-neutral-100 bg-blue-900 mb-[5px] text-lg">{index}</div>
+                                            <div className="flex items-center px-5 rounded-lg  h-[40px] text-neutral-100 bg-blue-900 mb-[5px] text-lg">{lookUp.documents[`${e}`][`${docName}`]}</div>
+                                        </div>
+                                    )
+                                }):false}   
+                            </section>
+                        </div>
+                        <div className="bg-blue-900 w-[20%] min-w-[300px] flex flex-col items-center gap-[5px] rounded-lg p-[20px]">
                             <span className="text-neutral-200 text-xl">Panou administrativ</span>
                             <div className="flex flex-row text-neutral-200 items-center gap-[10px] hover:cursor-pointer hover:bg-neutral-200 hover:text-blue-900 rounded-lg p-[5px]" onClick={goToUpload}>
                                 <Image src={uploadImage} width={25} height={25} alt="Urcare fișier" className="mix-blend-color-burn"/>
@@ -91,25 +112,6 @@ export default function Results({params}){
                             </div>
                         </div>
                     </div>
-                    <section id="documents" className="flex flex-col mx-10 justify-center bg-gray-100 p-[5px] rounded-lg mt-[10px]">
-                        {(lookUp.documents!=undefined)? Object.keys(lookUp.documents).map((e, index)=>{
-                            let docName="document_name";
-                            console.log(e, e["id"], e.docName, lookUp.documents, lookUp.documents[`${e}`][`${docName}`]);
-                            if(index%2==0)
-                                return(
-                                    <div key={index} className="flex w-[90%]">
-                                        <div className="flex items-center justify-center px-5 rounded-lg w-[40px] h-[40px] text-center mr-[5px] text-blue-900 bg-neutral-200 mb-[5px] text-lg">{index}</div>
-                                        <div className="flex items-center px-5 rounded-lg  h-[40px] text-blue-900 bg-neutral-200 mb-[5px] text-lg">{lookUp.documents[`${e}`][`${docName}`]}</div>
-                                    </div>
-                                )
-                            return(
-                                <div key={index} className="flex w-[90%]">
-                                    <div className="flex items-center justify-center px-5 rounded-lg w-[40px] h-[40px] text-center mr-[5px] text-neutral-100 bg-blue-900 mb-[5px] text-lg">{index}</div>
-                                    <div className="flex items-center px-5 rounded-lg  h-[40px] text-neutral-100 bg-blue-900 mb-[5px] text-lg">{lookUp.documents[`${e}`][`${docName}`]}</div>
-                                </div>
-                            )
-                        }):false}   
-                    </section>
                 </section>
             </div>
         )
