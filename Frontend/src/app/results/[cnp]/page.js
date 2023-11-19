a9038e133788f4bd211270f2fe302eb7e9d939b0
Răzvan <99297137+razvm91204@users.noreply.github.com>
2023-11-19 13:56:08 +0200

    Add files via upload

--- Frontend/src/app/results/[cnp]/page.js
+++ Frontend/src/app/results/[cnp]/page.js
@@ -45,7 +45,7 @@ export default function Results({params}){
                     const url = window.URL.createObjectURL(new Blob([response.data]));
                     const link = document.createElement('a');
                     link.href = url;
-                    link.setAttribute('download', `${lookUp.documents[docUUID]["document_name"]}`);
+                    link.setAttribute('download', `${lookUp.documents[e]["document_name"]}`);
                     link.click();
                 })
             }
