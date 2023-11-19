87145672f5ab0abc07777abd8385b4bf7203283f
raz <cloverofmisfortune04@gmail.com>
2023-11-19 10:58:30 +0200

    Adding selectable documents

--- Front-End/src/app/results/[cnp]/page.js
+++ Front-End/src/app/results/[cnp]/page.js
@@ -4,6 +4,7 @@ import Navbar from "../../Navbar/Navbar";
 import SearchCNP from "../../Search/search";
 import uploadImage from "../../../../public/upload.png"
 import downloadImage from "../../../../public/download.png"
+import results from './page.module.css'
 import Image from "next/image";
 
 import { useEffect, useState } from "react";
@@ -12,14 +13,14 @@ import { useEffect, useState } from "react";
 export default function Results({params}){
     const router=useRouter();
     const [lookUp, setLookUp]=useState({
-        person:{
-            id:"NmNlYjRiN2EtNTMyNi00ZTRlLWFlMTEtOGY5ZDNkYmZlMjUy",
-            cnp:"5040525350048",
-            nume:"Szabo",
-            prenume:"Aleks"
-        },
-        id:"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252",
-        "documents":{"cfc15005-af51-4632-9bd6-d5eca9f6d17b":{"id":"Y2ZjMTUwMDUtYWY1MS00NjMyLTliZDYtZDVlY2E5ZjZkMTdi","person":"VEVTVF9OQU1F","document_name":"TEST_HASH","document_hash":"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252"}}
+        // person:{
+        //     id:"NmNlYjRiN2EtNTMyNi00ZTRlLWFlMTEtOGY5ZDNkYmZlMjUy",
+        //     cnp:"5040525350048",
+        //     nume:"Szabo",
+        //     prenume:"Aleks"
+        // },
+        // id:"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252",
+        // "documents":{"cfc15005-af51-4632-9bd6-d5eca9f6d17b":{"id":"Y2ZjMTUwMDUtYWY1MS00NjMyLTliZDYtZDVlY2E5ZjZkMTdi","person":"VEVTVF9OQU1F","document_name":"TEST_HASH","document_hash":"6ceb4b7a-5326-4e4e-ae11-8f9d3dbfe252"}}
     
     });
 
@@ -44,7 +45,7 @@ export default function Results({params}){
       }    
 
     function downloadFiles(){
-
+        
     }
 
     useEffect(()=>{
@@ -56,6 +57,7 @@ export default function Results({params}){
                 e.json().then((json)=>{
                     fetch(`/api/people/${json.id}/documents`).then(response=>{
                         response.json().then(json2=>{
+                            
                             setLookUp({
                                 id:json.id,
                                 person:json.person,
@@ -66,6 +68,7 @@ export default function Results({params}){
                 });
             }).catch(err=>{
                 console.error(err);
+                router.replace('/');
             })
         }
     },[])
@@ -87,28 +90,32 @@ export default function Results({params}){
                                     if(index%2==0)
                                         return(
                                             <div key={index} className="flex w-[90%]">
-                                                <div className="flex items-center justify-center px-5 rounded-lg w-[40px] h-[40px] border-2 border-gray-300 text-center mr-[5px] text-blue-900 bg-neutral-200 mb-[5px] text-lg">{index}</div>
+                                                <div className="flex items-center justify-center rounded-lg w-[40px] h-[40px] border-2 border-gray-300 text-center 
+                                                mr-[5px] bg-neutral-200 mb-[5px] hover:cursor-pointer" onClick={(e)=>{console.log(e.target.classList.value="")}}></div>
                                                 <div className="flex items-center px-5 rounded-lg  h-[40px] text-blue-900 bg-neutral-200 border-2 border-gray-300  mb-[5px] text-lg">{lookUp.documents[`${e}`][`${docName}`]}</div>
                                             </div>
                                         )
                                     return(
                                         <div key={index} className="flex w-[90%]">
-                                            <div className="flex items-center justify-center px-5 rounded-lg w-[40px] h-[40px] text-center mr-[5px] text-neutral-100 bg-blue-900 mb-[5px] text-lg">{index}</div>
+                                            <div className="flex items-center justify-center rounded-lg w-[40px] h-[40px] text-center mr-[5px] bg-neutral-200 mb-[5px] text-lg"
+                                             onClick={(e)=>{console.log(e.target)}}></div>
                                             <div className="flex items-center px-5 rounded-lg  h-[40px] text-neutral-100 bg-blue-900 mb-[5px] text-lg">{lookUp.documents[`${e}`][`${docName}`]}</div>
                                         </div>
                                     )
                                 }):false}   
                             </section>
                         </div>
-                        <div className="bg-blue-900 w-[20%] min-w-[300px] flex flex-col items-center gap-[5px] rounded-lg p-[20px]">
+                        <div className="bg-blue-900 w-[20%] min-w-[300px] flex flex-col items-center justify-around rounded-lg p-[5px]">
                             <span className="text-neutral-200 text-xl">Panou administrativ</span>
-                            <div className="flex flex-row text-neutral-200 items-center gap-[10px] hover:cursor-pointer hover:bg-neutral-200 hover:text-blue-900 rounded-lg p-[5px]" onClick={goToUpload}>
-                                <Image src={uploadImage} width={25} height={25} alt="Urcare fișier" className="mix-blend-color-burn"/>
-                                <span className="text-lg">Încărcați documente</span>
-                            </div>
-                            <div className="flex flex-row text-neutral-200 items-center gap-[10px] hover:cursor-pointer hover:bg-neutral-200 hover:text-blue-900 rounded-lg p-[5px]" onClick={downloadFiles}>
-                                <Image src={downloadImage} width={25} height={25} alt="Descărcare fișier" className="mix-blend-color-burn"/>
-                                <span className="text-lg">Descărcați documente</span>
+                            <div>
+                                <div className="flex flex-row text-neutral-200 items-center justify-center gap-[10px] hover:cursor-pointer hover:bg-neutral-200 hover:text-blue-900 rounded-lg p-[5px]" onClick={goToUpload}>
+                                    <Image src={uploadImage} width={25} height={25} alt="Urcare fișier" className="mix-blend-color-burn"/>
+                                    <span className="text-lg">Încărcați documente</span>
+                                </div>
+                                <div className="flex flex-row text-neutral-200 items-center justify-center gap-[10px] hover:cursor-pointer hover:bg-neutral-200 hover:text-blue-900 rounded-lg p-[5px]" onClick={downloadFiles}>
+                                    <Image src={downloadImage} width={25} height={25} alt="Descărcare fișier" className="mix-blend-color-burn"/>
+                                    <span className="text-lg">Descărcați documente</span>
+                                </div>
                             </div>
                         </div>
                     </div>
