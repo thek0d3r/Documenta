917ef2f9aebe14e678bb373c1280e3cf44d4b0ca
Răzvan <99297137+razvm91204@users.noreply.github.com>
2023-11-19 13:43:24 +0200

    Add files via upload

--- Frontend/src/app/results/[cnp]/page.js
+++ Frontend/src/app/results/[cnp]/page.js
@@ -37,11 +37,22 @@ export default function Results({params}){
       }    
 
     function downloadFiles(){
-        // try{
-        //     fetch(`person/${params.uuid}/`)
-        // }
+        Object.keys(lookUp.documents).forEach((e)=>{
+            try{
+                fetch(`/api/people/${lookUp.id}/documents/${e["id"]}`).then(response=>{
+                    const url = window.URL.createObjectURL(new Blob([response.data]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', `${lookUp.documents[docUUID]["document_name"]}`);
+                    link.click();
+                })
+            }
+            catch{
+                console.log('Error');
+            }
+        
+        })
     }
-
     useEffect(()=>{
         if(validareCNP(params.cnp)==false)
             router.replace('/');
